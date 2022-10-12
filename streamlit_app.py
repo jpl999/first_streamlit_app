344ca583424eea09493c4655c2b6f9f0bef5b951
jpl999 <115171791+jpl999@users.noreply.github.com>
2022-10-12 15:39:46 -0400

    Moved Snowflake calls to function using with as

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,22 +43,29 @@ except URLError as e:
     streamlit.error()
         
 # don't run anything past here while we trobleshoot
-streamlit.stop()
-
-# Python Connector to Snowflake
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-# my_data_row = my_cur.fetchone()
-# streamlit.text("Hello from Snowflake:")
-# streamlit.text(my_data_row)
-
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_rows = my_cur.fetchall()
+# streamlit.stop()
 
 streamlit.header('The fruit load list contains:')
-streamlit.dataframe(my_data_rows)
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+        return my_cur.fetchall()
+
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()    
+    streamlit.dataframe(my_data_rows)
+## Python Connector to Snowflake
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#
+#my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+#my_data_rows = my_cur.fetchall()
+#
+#streamlit.header('The fruit load list contains:')
+#streamlit.dataframe(my_data_rows)
 
 # New Section to accept 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
