e159da2c179b3f051378159ec5e66e903415cab8
jpl999 <115171791+jpl999@users.noreply.github.com>
2022-10-12 13:56:12 -0400

    qualified the database and schema

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ my_cur = my_cnx.cursor()
 # streamlit.text("Hello from Snowflake:")
 # streamlit.text(my_data_row)
 
-my_cur.execute("select * from fruit_load_list;")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
