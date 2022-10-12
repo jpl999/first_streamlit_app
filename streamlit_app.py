8cd99111919366fb656bcae30e72b5df64f2c393
jpl999 <115171791+jpl999@users.noreply.github.com>
2022-10-12 14:37:19 -0400

    Added another text box

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,7 @@ my_data_rows = my_cur.fetchall()
 
 streamlit.header('The fruit load list contains:')
 streamlit.dataframe(my_data_rows)
+
+# New Section to accept 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Kiwi')
+streamlit.write('Thanks for adding ', add_my_fruit)
