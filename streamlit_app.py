54da4ee7105d763424208436e0575b8bddcfe9a6
jpl999 <115171791+jpl999@users.noreply.github.com>
2022-10-12 15:15:01 -0400

    typo on stop()

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we trobleshoot
-streamlit.stop;
+streamlit.stop()
 
 # Python Connector to Snowflake
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
