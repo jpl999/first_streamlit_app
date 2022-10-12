b30810d846e7a21405536ef39bb04773418b96b9
jpl999 <115171791+jpl999@users.noreply.github.com>
2022-10-12 14:23:56 -0400

    Fetch all rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,8 +46,7 @@ my_cur = my_cnx.cursor()
 # streamlit.text(my_data_row)
 
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 
 streamlit.header('The fruit load list contains:')
-# streamlit.text(my_data_row)
-streamlit.dataframe(my_data_row)
+streamlit.dataframe(my_data_rows)
